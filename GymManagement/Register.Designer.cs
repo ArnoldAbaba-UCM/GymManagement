c20917241746621daafc1f8f30420c609b318509
ArnoldAbaba-UCM <gmfireababa@gmail.com>
2026-04-28 23:16:11 +0800

    added login form - Ababa

---
+++ GymManagement/Register.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace GymManagement
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pl_createYourMembership = new System.Windows.Forms.Panel();
+            this.lbl_CreateYourMembership = new System.Windows.Forms.Label();
+            this.btn_Personal = new System.Windows.Forms.Button();
+            this.btn_Plan = new System.Windows.Forms.Button();
+            this.btn_Payment = new System.Windows.Forms.Button();
+            this.pl_Personal = new System.Windows.Forms.Panel();
+            this.lbl_FirstName = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txt_LastName = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txt_FirstName = new System.Windows.Forms.TextBox();
+            this.lbl_LastName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pl_createYourMembership.SuspendLayout();
+            this.pl_Personal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pl_createYourMembership
+            // 
+            this.pl_createYourMembership.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pl_createYourMembership.Controls.Add(this.lbl_CreateYourMembership);
+            this.pl_createYourMembership.Location = new System.Drawing.Point(0, 0);
+            this.pl_createYourMembership.Name = "pl_createYourMembership";
+            this.pl_createYourMembership.Size = new System.Drawing.Size(412, 34);
+            this.pl_createYourMembership.TabIndex = 1;
+            // 
+            // lbl_CreateYourMembership
+            // 
+            this.lbl_CreateYourMembership.AutoSize = true;
+            this.lbl_CreateYourMembership.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_CreateYourMembership.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_CreateYourMembership.Location = new System.Drawing.Point(12, 9);
+            this.lbl_CreateYourMembership.Name = "lbl_CreateYourMembership";
+            this.lbl_CreateYourMembership.Size = new System.Drawing.Size(177, 16);
+            this.lbl_CreateYourMembership.TabIndex = 0;
+            this.lbl_CreateYourMembership.Text = "Create your membership";
+            // 
+            // btn_Personal
+            // 
+            this.btn_Personal.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_Personal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Personal.Location = new System.Drawing.Point(0, 40);
+            this.btn_Personal.Name = "btn_Personal";
+            this.btn_Personal.Size = new System.Drawing.Size(74, 37);
+            this.btn_Personal.TabIndex = 7;
+            this.btn_Personal.Text = "1. Personal";
+            this.btn_Personal.UseVisualStyleBackColor = false;
+            // 
+            // btn_Plan
+            // 
+            this.btn_Plan.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_Plan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Plan.Location = new System.Drawing.Point(80, 40);
+            this.btn_Plan.Name = "btn_Plan";
+            this.btn_Plan.Size = new System.Drawing.Size(74, 37);
+            this.btn_Plan.TabIndex = 8;
+            this.btn_Plan.Text = "2. Plan";
+            this.btn_Plan.UseVisualStyleBackColor = false;
+            // 
+            // btn_Payment
+            // 
+            this.btn_Payment.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_Payment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Payment.Location = new System.Drawing.Point(160, 40);
+            this.btn_Payment.Name = "btn_Payment";
+            this.btn_Payment.Size = new System.Drawing.Size(74, 37);
+            this.btn_Payment.TabIndex = 9;
+            this.btn_Payment.Text = "3. Payment";
+            this.btn_Payment.UseVisualStyleBackColor = false;
+            // 
+            // pl_Personal
+            // 
+            this.pl_Personal.Controls.Add(this.label1);
+            this.pl_Personal.Controls.Add(this.lbl_LastName);
+            this.pl_Personal.Controls.Add(this.lbl_FirstName);
+            this.pl_Personal.Controls.Add(this.textBox5);
+            this.pl_Personal.Controls.Add(this.textBox4);
+            this.pl_Personal.Controls.Add(this.textBox3);
+            this.pl_Personal.Controls.Add(this.txt_LastName);
+            this.pl_Personal.Controls.Add(this.comboBox1);
+            this.pl_Personal.Controls.Add(this.txt_FirstName);
+            this.pl_Personal.Location = new System.Drawing.Point(0, 83);
+            this.pl_Personal.Name = "pl_Personal";
+            this.pl_Personal.Size = new System.Drawing.Size(412, 263);
+            this.pl_Personal.TabIndex = 10;
+            // 
+            // lbl_FirstName
+            // 
+            this.lbl_FirstName.AutoSize = true;
+            this.lbl_FirstName.Location = new System.Drawing.Point(38, 33);
+            this.lbl_FirstName.Name = "lbl_FirstName";
+            this.lbl_FirstName.Size = new System.Drawing.Size(60, 13);
+            this.lbl_FirstName.TabIndex = 10;
+            this.lbl_FirstName.Text = "First Name:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(140, 153);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(218, 26);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(140, 121);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(218, 26);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(140, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(218, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // txt_LastName
+            // 
+            this.txt_LastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_LastName.Location = new System.Drawing.Point(140, 57);
+            this.txt_LastName.Name = "txt_LastName";
+            this.txt_LastName.Size = new System.Drawing.Size(218, 26);
+            this.txt_LastName.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(140, 185);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 28);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // txt_FirstName
+            // 
+            this.txt_FirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_FirstName.Location = new System.Drawing.Point(140, 25);
+            this.txt_FirstName.Name = "txt_FirstName";
+            this.txt_FirstName.Size = new System.Drawing.Size(218, 26);
+            this.txt_FirstName.TabIndex = 0;
+            // 
+            // lbl_LastName
+            // 
+            this.lbl_LastName.AutoSize = true;
+            this.lbl_LastName.Location = new System.Drawing.Point(38, 65);
+            this.lbl_LastName.Name = "lbl_LastName";
+            this.lbl_LastName.Size = new System.Drawing.Size(61, 13);
+            this.lbl_LastName.TabIndex = 11;
+            this.lbl_LastName.Text = "Last Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Last Name:";
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 389);
+            this.Controls.Add(this.pl_Personal);
+            this.Controls.Add(this.btn_Payment);
+            this.Controls.Add(this.btn_Plan);
+            this.Controls.Add(this.btn_Personal);
+            this.Controls.Add(this.pl_createYourMembership);
+            this.Name = "Register";
+            this.Text = "Register";
+            this.pl_createYourMembership.ResumeLayout(false);
+            this.pl_createYourMembership.PerformLayout();
+            this.pl_Personal.ResumeLayout(false);
+            this.pl_Personal.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pl_createYourMembership;
+        private System.Windows.Forms.Label lbl_CreateYourMembership;
+        private System.Windows.Forms.Button btn_Personal;
+        private System.Windows.Forms.Button btn_Plan;
+        private System.Windows.Forms.Button btn_Payment;
+        private System.Windows.Forms.Panel pl_Personal;
+        private System.Windows.Forms.Label lbl_FirstName;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txt_LastName;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txt_FirstName;
+        private System.Windows.Forms.Label lbl_LastName;
+        private System.Windows.Forms.Label label1;
+    }
+}
